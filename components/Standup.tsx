1e80847697486034b14d72c091dfe25f29f34b1c
Jan Klimo <jan.klimo@gmail.com>
2021-09-14 15:22:26 +0700

    Use a link

--- components/Standup.tsx
+++ components/Standup.tsx
@@ -7,72 +7,74 @@ const height = 120;
 const data = [
   {
     name: "Chris",
-    description: "#e0c3fc → #8ec5fc",
     css: "linear-gradient(135deg, #e0c3fc 0%, #8ec5fc 100%)",
     height,
   },
   {
     name: "Jan",
-    description: "#5ee7df → #b490ca",
-    css: "linear-gradient(135deg, #5ee7df 0%, #b490ca 100%)",
+    css: "linear-gradient(135deg, #ed6fbb 0%, #ff9378 100%)",
     height,
   },
   {
     name: "Noey",
-    description: "#d299c2 → #fef9d7",
     css: "linear-gradient(135deg, #d299c2 0%, #fef9d7 100%)",
     height,
   },
   {
     name: "Oo",
-    description: "#f6d365 → #fda085",
     css: "linear-gradient(120deg, #f6d365 0%, #fda085 100%)",
     height,
   },
   {
     name: "Pin",
-    description: "#96fbc4 → #f9f586",
     css: "linear-gradient(to top, #96fbc4 0%, #f9f586 100%)",
     height,
   },
 ];
 
 const Standup: FC = () => {
-  const [rows, set] = useState(data);
-  useEffect(() => {
-    const t = setInterval(() => set(shuffle), 2000);
-    return () => clearInterval(t);
-  }, []);
+  const [items, setItems] = useState(data);
 
   let height = 0;
   const transitions = useTransition(
-    rows.map((data) => ({ ...data, y: (height += data.height) - data.height })),
+    items.map((data) => ({
+      ...data,
+      y: (height += data.height) - data.height,
+    })),
     {
-      key: (item: any) => item.name,
-      from: { height: 0, opacity: 0 },
-      leave: { height: 0, opacity: 0 },
-      enter: ({ y, height }) => ({ y, height, opacity: 1 }),
-      update: ({ y, height }) => ({ y, height }),
+      key: (person: any) => person.name,
+      from: { opacity: 0 },
+      leave: { opacity: 0 },
+      enter: ({ y }) => ({ y, opacity: 1 }),
+      update: ({ y }) => ({ y }),
     }
   );
 
   return (
-    <div className="relative w-60 mx-auto" style={{ height }}>
-      {transitions((style, item, t, index) => (
-        <animated.div
-          className="absolute w-full"
-          style={{ zIndex: data.length - index, ...style }}
-        >
-          <div className="relative w-full h-full p-4 bg-cover">
-            <div
-              className="relative bottom-0 left-0 w-full h-full rounded-md shadow-md"
-              style={{ backgroundImage: item.css }}
-            />
-            {item.name}
-          </div>
-        </animated.div>
-      ))}
-    </div>
+    <>
+      <a href="#" onClick={() => setItems(shuffle)}>
+        Shuffle
+      </a>
+      <div className="relative w-60 mx-auto" style={{ height }}>
+        {transitions((style, item, _, index) => (
+          <animated.div
+            className="absolute w-full"
+            style={{ zIndex: data.length - index, ...style }}
+          >
+            <div className="relative p-4 bg-cover">
+              <div
+                className="relative flex justify-center items-center bottom-0 left-0 w-full rounded-md shadow-md h-20"
+                style={{ backgroundImage: item.css }}
+              >
+                <p className="text-lg text-gray-700 font-bold drop-shadow-lg">
+                  {item.name}
+                </p>
+              </div>
+            </div>
+          </animated.div>
+        ))}
+      </div>
+    </>
   );
 };
 
